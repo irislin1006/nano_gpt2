c8d756a17bea6d6f48edcc20d04f4e7b574ea325
irislin1006 <iris19132@gmail.com>
2023-12-07 09:41:47 -0600

    some minor bug in generation

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ from src.models.gpt2.model import GPT, GPTConfig
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'out'
-eval_interval = 2000
+eval_interval = 3000
 log_interval = 1
 eval_iters = 100
 eval_only = False # if True, script exits right after the first eval
@@ -42,15 +42,15 @@ dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
 # adam optimizer
 learning_rate = 6e-4 # max learning rate
-max_iters = 100 # total number of training iterations; (TODO) increase to 600000
+max_iters = 600000 # total number of training iterations
 weight_decay = 1e-1
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
-warmup_iters = 20 # how many steps to warm up for; (TODO) increase to 2000
-lr_decay_iters = 100 # should be ~= max_iters per Chinchilla; (TODO) increase to 600000
+warmup_iters = 2000 # how many steps to warm up for
+lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
 min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # system
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
